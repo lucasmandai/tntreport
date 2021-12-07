6f7ae43d7c5adfe8cab15303eaadef4e4c96e6a4
lucas <lucas.mitsuo@tntedu.com.br>
2021-12-07 17:12:36 -0300

    first commit

---
+++ src/main/java/br/com/tnttec/TntReportApplication.java
@@ -0,0 +1,20 @@
+package br.com.tnttec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@RestController
+public class TntReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TntReportApplication.class, args);
+	}
+
+	@GetMapping(value = "/hello")
+	public String helloWorld(){
+		return "Welcome to the new age 2";
+	}
+}
